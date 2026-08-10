a76aa890cc3451617229cea899622626b99caac8
arkj5107-boop <arkj5107@gmail.com>
2026-08-11 02:22:54 +0800

    Create articles.js

---
+++ articles.js
@@ -0,0 +1,84 @@
+const articles = [
+
+    {
+        title: "Ang Salimbay Nanghikayat!",
+        category: "Balita",
+        description: "Kampanya para sa mga batang mamamahayag ng Sucat Senior High School.",
+        keywords: "salimbay nanghikayat kampanya mamamahayag pamamahayag pahayagan journalism",
+        image: "Images/Messenger_creation_ADE8D76A-1078-418E-B254-0D737A6C51AA.jpeg",
+        url: "nanghikayat.html"
+    },
+
+    {
+        title: "Bagong Performing Arts Hall Pinasinayaan sa SSHS",
+        category: "Balita",
+        description: "Pormal nang binuksan ang bagong Performing Arts Hall ng Sucat Senior High School.",
+        keywords: "performing arts hall where art comes alive sining pva cypheria ribbon cutting",
+        image: "Messenger_creation_CC6E278C-72C7-4F31-934B-DE99E607B71E.jpeg",
+        url: "performinghall.html"
+    },
+
+    {
+        title: "Career Guidance ng SIM Diamond",
+        category: "Balita",
+        description: "Career Support Program para sa mga mag-aaral ng Sucat Senior High School.",
+        keywords: "career guidance career support sim diamond soroptimist dream it be it scholarship",
+        image: "Messenger_creation_EADC6BF3-7E9F-483E-ADF5-29787308405F.jpeg",
+        url: "career.html"
+    },
+
+    {
+        title: "MUNwalk SNEAKER Turn Over Ceremony sa SSHS",
+        category: "Balita",
+        description: "Pamamahagi ng MUNwalk SNEAKER para sa mga mag-aaral ng Sucat Senior High School.",
+        keywords: "munwalk sneaker sapatos muntinlupa mayor ruffy biazon school supplies",
+        image: "Images/received_2517081808793748.jpeg",
+        url: "munwalk.html"
+    },
+
+    {
+        title: "Ang Salimbay, Nagbukas ng Pinto Para sa mga Bagong Mamamahayag",
+        category: "Balita",
+        description: "Pagpupulong at pagsusulit para sa mga bagong mamamahayag ng Ang Salimbay.",
+        keywords: "salimbay bagong mamamahayag ang katha nheil patrick raymunde dspc radio broadcasting tv broadcasting",
+        image: "Images/received_1259784629511883.jpeg",
+        url: "pagpupulong.html"
+    },
+
+    {
+        title: "Nakakas ilaw na Liwanag ng Pag-asa",
+        category: "Balita",
+        description: "Paggunita ng Sucat Senior High School kay Jeremy A. Cortez.",
+        keywords: "jeremy cortez candle lighting scout sr scout paggunita sucat",
+        image: "Images/Messenger_creation_1695648618385477.jpeg",
+        url: "jeremy.html"
+    },
+
+    {
+        title: "Lingkod-Bayan o Lingkod Ba ’yan?",
+        category: "Editoryal",
+        description: "Editoryal tungkol sa serbisyo publiko, pananagutan at pagganap ng mga opisyal.",
+        keywords: "lingkod bayan lingkod editoryal senado senador senate attendance serbisyo publiko",
+        image: "",
+        url: "lingkod.html"
+    },
+
+    {
+        title: "EskuweLAGIM",
+        category: "Editoryal",
+        description: "Editoryal tungkol sa kaligtasan, disiplina at karahasan sa paaralan.",
+        keywords: "eskuwelagim paaralan seguridad karahasan edukasyon disiplina kabataan",
+        image: "",
+        url: "lagim.html"
+    },
+
+    {
+        title: "Basang Kalsada, Masayang Parada: Pista ni Sto. Domingo de Guzman, Hindi Nagpatinag sa Ulan",
+        category: "Lathalain",
+        description: "Pista ni Sto. Domingo de Guzman sa Sucat na hindi nagpatinag sa ulan.",
+        keywords: "basang kalsada masayang parada sto domingo de guzman pista ulan karakol sucat muntinlupa",
+        image: "",
+        url: "fiesta-sto-domingo.html"
+    }
+
+];
