b047c03055a42260f33bb76796c74955fe8230c5
arkj5107-boop <arkj5107@gmail.com>
2026-08-11 02:36:10 +0800

    Create articles.js

---
+++ articles.js
@@ -0,0 +1,102 @@
+const articles = [
+
+    {
+        title: "Bagong Performing Arts Hall Pinasinayaan sa SSHS",
+        description: "Pormal nang binuksan ang bagong Performing Arts Hall ng Sucat Senior High School...",
+        category: "Balita",
+        date: "Hunyo 22, 2026",
+        author: "Wilmarie A. Gonzales",
+        keywords: "performing arts hall SSHS sining pasinaya",
+        image: "Messenger_creation_CC6E278C-72C7-4F31-934B-DE99E607B71E.jpeg",
+        url: "performinghall.html"
+    },
+
+    {
+        title: "Career Guidance ng SIM Diamond, Dinaluhan ng mga Kababaihang Mag-aaral ng SSHS",
+        description: "Nagkaroon ng Career Support Program para sa mga mag-aaral ng Sucat Senior High School...",
+        category: "Balita",
+        date: "Hunyo 25, 2026",
+        author: "Wilmarie A. Gonzales",
+        keywords: "career guidance SIM Diamond DIBI scholarship career",
+        image: "Messenger_creation_EADC6BF3-7E9F-483E-ADF5-29787308405F.jpeg",
+        url: "career.html"
+    },
+
+    {
+        title: "Kampanya Para sa mga Batang Mamamahayag, Inilunsad ng Ang Salimbay",
+        description: "Bilang paghahanda sa pagpili ng mga bagong miyembro ng opisyal na publikasyong Filipino ng paaralan...",
+        category: "Balita",
+        date: "2026",
+        author: "Ang Salimbay",
+        keywords: "mamamahayag journalism Ang Salimbay kampanya",
+        image: "Messenger_creation_ADE8D76A-1078-418E-B254-0D737A6C51AA.jpeg",
+        url: "nanghikayat.html"
+    },
+
+    {
+        title: "Ang Salimbay, Nagbukas ng Pinto Para sa mga Bagong Mamamahayag",
+        description: "Nagkaroon ng pagpupulong ang Ang Salimbay bilang daan sa paghahanap ng mga panibagong miyembro ng pahayagan.",
+        category: "Balita",
+        date: "Hulyo 18, 2026",
+        author: "Wilmarie Gonzales",
+        keywords: "Ang Salimbay bagong mamamahayag journalism pagpupulong",
+        image: "Messenger_creation_1695648618385477.jpeg",
+        url: "pagpupulong.html"
+    },
+
+    {
+        title: "MUNwalk SNEAKER Turn Over Ceremony sa Sucat Senior High School",
+        description: "Ipinamahagi ang mga libreng sapatos ng Pamahalaang Lungsod ng Muntinlupa para sa mga mag-aaral.",
+        category: "Balita",
+        date: "Hulyo 15, 2026",
+        author: "Wilmarie Gonzales",
+        keywords: "MUNwalk sneaker sapatos Muntinlupa SSHS",
+        image: "Messenger_creation_2777425902629198.jpeg",
+        url: "munwalk.html"
+    },
+
+    {
+        title: "Nakakasilaw na Liwanag ng Pag-asa: Sucat Senior High School, Nagkaisa sa Paggunita kay Jeremy A. Cortez",
+        description: "Nagkaisa ang pamayanan ng Sucat Senior High School sa isang candle-lighting ceremony upang parangalan ang buhay ni Jeremy.",
+        category: "Lathalain",
+        date: "2026",
+        author: "Sean Michael Bandilla at Rashad Arkjan Miranda",
+        keywords: "Jeremy Cortez SSHS paggunita candle lighting",
+        image: "received_1259784629511883.jpeg",
+        url: "jeremy.html"
+    },
+
+    {
+        title: "Lingkod-Bayan o Lingkod ba 'yan?",
+        description: "Ang serbisyo publiko ay hindi entablado ng kapangyarihan, kundi larangan ng pananagutan.",
+        category: "Editoryal",
+        date: "2026",
+        author: "Kenneth Tuscano",
+        keywords: "lingkod bayan Senado senador attendance serbisyo publiko",
+        image: "received_1552567129154682.jpeg",
+        url: "lingkod.html"
+    },
+
+    {
+        title: "EskuweLAGIM",
+        description: "Ang paaralan ay dapat pugad ng pangarap, hindi puntod ng pag-asa; tahanan ng kaalaman, hindi larangan ng karahasan.",
+        category: "Editoryal",
+        date: "2026",
+        author: "Kenneth Tuscano",
+        keywords: "EskuweLAGIM paaralan kaligtasan karahasan seguridad",
+        image: "received_2517081808793748.jpeg",
+        url: "lagim.html"
+    },
+
+    {
+        title: "Basang Kalsada, Masayang Parada: Pista ni Sto. Domingo de Guzman, Hindi Nagpatinag sa Ulan",
+        description: "Hindi nagpahuli ang mga taga-Sucat sa pagdiriwang ng pista ni Sto. Domingo de Guzman sa kabila ng ulan.",
+        category: "Lathalain",
+        date: "Agosto 8, 2026",
+        author: "Rashad Arkjan Miranda",
+        keywords: "Sto Domingo de Guzman pista fiesta Sucat ulan karakol parada",
+        image: "Messenger_creation_C6572A7C-EC18-4B97-A68C-32D28DF9E756.jpeg",
+        url: "fiesta-sto-domingo.html"
+    }
+
+];
